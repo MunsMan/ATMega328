e92cb0457eb434ec43b1e573c1ed2037fb98e8fa
Hendrik Paul Munske <csaw8595@zid-gpl.uibk.ac.at>
2021-05-17 22:21:11 +0200

    Updating Parser

--- compiler1/module/Parser/Parser.py
+++ compiler1/module/Parser/Parser.py
@@ -1,18 +1,27 @@
+from compiler1.module.Instructions.commands import mapCommmands
 from typing import Callable, List, Tuple
 
 
 class Parser:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, labelRef) -> None:
+        self.labelRef = labelRef
 
     def parseLine(self, line: str):
         line: List[str] = line.split(" ")
         op, cond, flag = self.parseOpcode(line[0])
+        rd = line[1]
+        rr = line[2]
+        mapCommmands(op, cond, flag, rd, rd, self.labelRef)
 
     def parseOpcode(self, opcode: str):
-        if len(opcode) == 3:
-            return opcode.upper(), "", ""
-        else:
-            # ToDo: implement parse Condition and Flag
-            print("Error ParseOpcode")
-            return "NOP", "", ""
+
+        case = {
+            3: (opcode.upper(), "", ""),
+            4: (opcode[0:2].upper(), opcode[2:4].upper(), ""),
+            5: (opcode[0:3].upper(), opcode[2:4].upper(), ""),
+        }
+        if len(opcode) in case:
+            return case[len(opcode)]
+
+        print("Error ParseOpcode")
+        return "NOP", "", ""
