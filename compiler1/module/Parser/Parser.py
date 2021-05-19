43a91749814e3bd0083911970f90e3d2a818a209
MunsMan <hendrik.munske@me.com>
2021-05-19 18:14:23 +0000

    Add parseBlock

--- compiler1/module/Parser/Parser.py
+++ compiler1/module/Parser/Parser.py
@@ -1,17 +1,37 @@
-from compiler1.module.Instructions.commands import mapCommmands
-from typing import Callable, List, Tuple
+from module.Instructions import instructions
+from ..Instructions.commands import CommandArgs, mapCommmands
+from typing import Any, Callable, Dict, List, Tuple
 
 
 class Parser:
     def __init__(self, labelRef) -> None:
         self.labelRef = labelRef
+        self.label = "main"
 
-    def parseLine(self, line: str):
+    def parseLine(self, line: str, numLine: int) -> Tuple[int, Callable[[], List[int]]]:
         line: List[str] = line.split(" ")
         op, cond, flag = self.parseOpcode(line[0])
         rd = line[1]
         rr = line[2]
-        mapCommmands(op, cond, flag, rd, rd, self.labelRef)
+        args = CommandArgs(op, cond, flag, rd, rr, self.labelRef, numLine, self.label)
+        return mapCommmands(args)
+
+    def parseBlock(self, labelName:str, label: List[str]) -> Tuple[int, List[Tuple[int, Callable[[], List[int]]]]]:
+        self.label = labelName 
+        numLine = 0
+        instructions = []
+        numInstructions = 0
+        for line in label:
+            res = self.parseLine(line, numLine)
+            numInstructions += res[0]
+            instructions.append(res)
+            numLine += 1
+        return numInstructions, instructions
+
+    def parseLabels(self, labels: Dict[str, List[Any]]):
+        for label in labels:
+            labels[label] = self.parseBlock(label, labels[label])
+        return labels
 
     def parseOpcode(self, opcode: str):
 
