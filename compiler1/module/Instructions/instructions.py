472fc8fc7edc98d9eb5e42225d6721ef853bb1a2
MunsMan <hendrik.munske@me.com>
2021-05-19 18:14:48 +0000

    add new Instructions

--- compiler1/module/Instructions/instructions.py
+++ compiler1/module/Instructions/instructions.py
@@ -1,5 +1,6 @@
+from . import twoOp
 from typing import Callable
-from compiler1.module.errorHandling.error import throwError
+from ..errorHandling.error import throwError
 
 
 def mapInstructions(opcode: str) -> Callable:
@@ -8,15 +9,56 @@ def mapInstructions(opcode: str) -> Callable:
     throwError(4, True, (opcode))
 
 
-def ldi(rd: int, const: int):
-    pass
+def ldi(rd: int, k: int) -> int:
+    opcode = 0xE000
+    kh = (k & 0xF0) << 4
+    kl = k & 0xF
+    dest = (rd - 16) << 4
+    return opcode + kh + dest + kl
+
+def mov(rd: int, rr: int) -> int:
+    return (0b001011 << 10) + twoOp(rd, rr)
 
 
-def mov(rd: int, rr: int):
-    pass
+def add(rd: int, rn: int) -> int:
+    return (3 << 10) + twoOp(rd, rn)
+
+
+def logicalAndIm(rd: int, k: int) -> int:
+    opcode = 0x7 << 12
+    kh = (k & 0xF0) << 4
+    kl = (k & 0xF)
+    d = rd << 4
+    return opcode + kh + d + kl
+
+
+def shiftRight(rd: int) -> int:
+    opcode = 0x9405
+    d = rd << 4
+    return opcode + d
+
+
+def clearBitSREG(rd: int) -> int:
+    opcode = 0x9488
+    s = rd << 4
+    return opcode + s
+
+
+def loadBitT(rd: int, rr: str) -> int:
+    opcode = 0xF800
+    rd = rd << 4
+    return opcode + rd + rr
+
+
+def brbc(rd: int, rr: int):
+    opcode = 0xF400
+    k = rr << 3
+    return opcode + k + rd
 
 
 InstructionsMap = {
     "ldi": ldi,
-    "mov": mov
+    "mov": mov,
+    "add": add,
+    "brbc": brbc,
 }
