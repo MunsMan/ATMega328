8a7db680fbe1ac4f1e17c1c2643b188fccea0342
MunsMan <hendrik.munske@me.com>
2021-05-29 10:25:14 +0200

    solving branch test problem

--- compiler1/module/Instructions/tests/test_branch.py
+++ compiler1/module/Instructions/tests/test_branch.py
@@ -2,8 +2,8 @@ from pytest_mock import MockerFixture
 import pytest
 
 from .helper import mock_exit
-from ..branch import BRBS
-from ..instructions import brbs
+from ..branch import BRBS, BRBC
+from ..instructions import brbc, brbs
 from ..helper import twoComplement
 from .. import helper
 from .. import instructions as Instructions
@@ -15,13 +15,63 @@ def test_BRBC_validImmediate(mocker: MockerFixture):
     offsets = range(-64, 64)
     for flag in flags:
         for offset in offsets:
+            numInstructions, instructions = BRBC(flag, offset, None)
+            assert(numInstructions == 1)
+            assert(instructions() == [brbc(flag, twoComplement(offset, 7))])
+    mocker_throwError.assert_not_called()
+
+
+def test_BRBC_invalidImmediate(mocker: MockerFixture):
+    mocker_throwError = mocker.patch.object(helper, "throwError")
+    mocker_throwError.side_effect = mock_exit
+    flags = range(0, 8)
+    invalidOffsets = [-65, 64]
+    for flag in flags:
+        for offset in invalidOffsets:
+            with pytest.raises(SystemExit):
+                BRBC(flag, offset, None)
+            mocker_throwError.assert_called_once_with(
+                10, True, (offset, 7))
+            mocker_throwError.reset_mock()
+
+
+def test_BRBC_negativFlag(mocker: MockerFixture):
+    mocker_throwError = mocker.patch.object(helper, "throwError")
+    mocker_throwError.side_effect = mock_exit
+    invalidFlags = -1
+    validOffset = 1
+    with pytest.raises(SystemExit):
+        BRBC(invalidFlags, validOffset, None)
+    mocker_throwError.assert_called_once_with(
+        6, True, invalidFlags)
+    mocker_throwError.reset_mock()
+
+
+def test_BRBC_overflowFlag(mocker: MockerFixture):
+    mocker_throwError = mocker.patch.object(helper, "throwError")
+    mocker_throwError.side_effect = mock_exit
+    invalidFlags = 8
+    validOffset = 1
+    with pytest.raises(SystemExit):
+        BRBC(invalidFlags, validOffset, None)
+    mocker_throwError.assert_called_once_with(
+        7, True, (invalidFlags, invalidFlags.bit_length(), 3))
+    mocker_throwError.reset_mock()
+
+
+def test_BRBS_validImmediate(mocker: MockerFixture):
+    mocker_throwError = mocker.patch.object(Instructions, "throwError")
+    flags = range(0, 8)
+    offsets = range(-64, 64)
+    for flag in flags:
+        for offset in offsets:
             numInstructions, instructions = BRBS(flag, offset, None)
             assert(numInstructions == 1)
             assert(instructions() == [brbs(flag, twoComplement(offset, 7))])
     mocker_throwError.assert_not_called()
 
 
-def test_BRBC_invalidImmediate(mocker: MockerFixture):
+def test_BRBS_invalidImmediate(mocker: MockerFixture):
     mocker_throwError = mocker.patch.object(helper, "throwError")
     mocker_throwError.side_effect = mock_exit
     flags = range(0, 8)
@@ -35,14 +85,25 @@ def test_BRBC_invalidImmediate(mocker: MockerFixture):
             mocker_throwError.reset_mock()
 
 
-def test_BRBC_invalidFlag(mocker: MockerFixture):
+def test_BRBS_negativFlag(mocker: MockerFixture):
     mocker_throwError = mocker.patch.object(helper, "throwError")
     mocker_throwError.side_effect = mock_exit
-    invalidFlags = [-1, 8]
+    invalidFlags = -1
     validOffset = 1
-    for flag in invalidFlags:
-        with pytest.raises(SystemExit):
-            BRBS(flag, validOffset, None)
-        mocker_throwError.assert_called_once_with(
-            7, True, (flag, flag.bit_length(), 3))
-        mocker_throwError.reset_mock()
+    with pytest.raises(SystemExit):
+        BRBS(invalidFlags, validOffset, None)
+    mocker_throwError.assert_called_once_with(
+        6, True, invalidFlags)
+    mocker_throwError.reset_mock()
+
+
+def test_BRBS_overflowFlag(mocker: MockerFixture):
+    mocker_throwError = mocker.patch.object(helper, "throwError")
+    mocker_throwError.side_effect = mock_exit
+    invalidFlags = 8
+    validOffset = 1
+    with pytest.raises(SystemExit):
+        BRBS(invalidFlags, validOffset, None)
+    mocker_throwError.assert_called_once_with(
+        7, True, (invalidFlags, invalidFlags.bit_length(), 3))
+    mocker_throwError.reset_mock()
