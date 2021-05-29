e2c6d609c049c8741499b0773dceafa3b572daee
MunsMan <hendrik.munske@me.com>
2021-05-29 10:24:40 +0200

    refactor Flags

---
+++ compiler1/module/Instructions/Flags.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Flags(Enum):
+    C = 0
+    Z = 1
+    N = 2
+    V = 3
+    S = 4
+    H = 5
+    T = 6
+    I = 7
--- compiler1/module/Instructions/branch.py
+++ compiler1/module/Instructions/branch.py
@@ -1,107 +1,100 @@
-from ..Instructions.helper import checkImmediateSize, twoComplement
+from ..Instructions.helper import checkFlag, twoComplement
 from ..Instructions.instructions import mapInstructions
 from typing import Callable, Union
-from enum import Enum
+from . import Flags
 
 INSTRUCTIONS_LEN = 1
 
 
-class Flag(Enum):
-    C = 0
-    Z = 1
-    N = 2
-    V = 3
-    S = 4
-    H = 5
-    T = 6
-    I = 7
-
-
 def BRCC(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.C, offset, labelRef)
+    return BRBC(Flags.C, offset, labelRef)
 
 
 def BRCS(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.C, offset, labelRef)
+    return BRBS(Flags.C, offset, labelRef)
 
 
 def BREQ(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.Z, offset, labelRef)
+    return BRBS(Flags.Z, offset, labelRef)
 
 
 def BRGE(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.S, offset, labelRef)
+    return BRBC(Flags.S, offset, labelRef)
 
 
 def BRHC(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.H, offset, labelRef)
+    return BRBC(Flags.H, offset, labelRef)
 
 
 def BRHS(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.H, offset, labelRef)
+    return BRBS(Flags.H, offset, labelRef)
 
 
 def BRID(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.I, offset, labelRef)
+    return BRBC(Flags.I, offset, labelRef)
 
 
 def BRIE(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.I, offset, labelRef)
+    return BRBS(Flags.I, offset, labelRef)
 
 
 def BRLO(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.C, offset, labelRef)
+    return BRBS(Flags.C, offset, labelRef)
 
 
 def BRLT(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.S, offset, labelRef)
+    return BRBS(Flags.S, offset, labelRef)
 
 
 def BRMI(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.N, offset, labelRef)
+    return BRBS(Flags.N, offset, labelRef)
 
 
 def BRNE(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.Z, offset, labelRef)
+    return BRBC(Flags.Z, offset, labelRef)
 
 
 def BRPL(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.N, offset, labelRef)
+    return BRBC(Flags.N, offset, labelRef)
 
 
 def BRSH(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.C, offset, labelRef)
+    return BRBC(Flags.C, offset, labelRef)
 
 
 def BRTC(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.T, offset, labelRef)
+    return BRBC(Flags.T, offset, labelRef)
 
 
 def BRTS(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.T, offset, labelRef)
+    return BRBS(Flags.T, offset, labelRef)
 
 
 def BRVC(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBC(Flag.V, offset, labelRef)
+    return BRBC(Flags.V, offset, labelRef)
 
 
 def BRVS(offset: Union[str, int], labelRef: Callable[[], int]):
-    return BRBS(Flag.V, offset, labelRef)
+    return BRBS(Flags.V, offset, labelRef)
 
 
 def BRBC(flag: int, offset: Union[str, int], labelRef: Callable[[], int]):
+    checkFlag(flag)
     if not str(offset).replace("-", "").isdigit():
         return INSTRUCTIONS_LEN, lambda: [mapInstructions('brbc')(
             flag, labelRef())]
     else:
-        return INSTRUCTIONS_LEN, lambda: [mapInstructions('brbc')(flag, int(offset))]
+        instruction = mapInstructions('brbc')(
+            flag, twoComplement(int(offset), 7))
+        return INSTRUCTIONS_LEN, lambda: [instruction]
 
 
 def BRBS(flag: int, offset: Union[str, int], labelRef: Callable[[], int]):
-    checkImmediateSize(flag, 3)
+    checkFlag(flag)
     if not str(offset).replace("-", "").isdigit():
         return INSTRUCTIONS_LEN, lambda: [mapInstructions('brbs')(
             flag, twoComplement(labelRef(), 7))]
     else:
-        return INSTRUCTIONS_LEN, lambda: [mapInstructions('brbs')(
-            flag, twoComplement(int(offset), 7))]
+        instruction = mapInstructions('brbs')(
+            flag, twoComplement(int(offset), 7))
+        return INSTRUCTIONS_LEN, lambda: [instruction]
