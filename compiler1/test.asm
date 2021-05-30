76acdbe68bf1a2adc95953b488f8fdb5bae5cd1b
MunsMan <hendrik.munske@me.com>
2021-05-30 14:39:01 +0000

    state

--- compiler1/test.asm
+++ compiler1/test.asm
@@ -7,17 +7,20 @@ jump:
     ADD r0 #1
     ADD r0 r1
     ADD r1 r2
-    BRBC 1 hi
+    BRBC 1, hi; // test
 add1:
-    ADD r0, 1;
+    ADD r0 1;
 
 main:
-    MOV r0 #8
+    ASR r5
+    ASR r5,5
+    MOV r0      #8
     MOV r1 #1
+    MOV r4 #2;
     AND r2 r0
     BREQ jump
-    ADD X, 63
-    ADD Y, 63
-    ADD Z, 63
+    ADD X 63
+    ADD Y 63
+    ADD Z 63
     ADD r24:r25 63
     ADD r24: 63
