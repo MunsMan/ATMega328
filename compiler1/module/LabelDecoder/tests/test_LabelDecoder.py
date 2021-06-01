e6574dad238ac73cace2a35e7241eeb00a0cd422
MunsMan <hendrik.munske@me.com>
2021-06-01 19:16:26 +0000

    testing LabelDecoder

---
+++ compiler1/module/LabelDecoder/tests/test_LabelDecoder.py
@@ -0,0 +1,31 @@
+from unittest.mock import Mock
+from ..LabelDecoder import LabelDecoder
+from typing import List
+from pytest_mock import MockerFixture
+
+
+def zipLines(lines: List[str]):
+    return list(zip(range(1, len(lines)+1), lines))
+
+
+def test_scanLabels(mocker: MockerFixture):
+    mocker.patch.object(LabelDecoder, "_parse")
+    rawInput = zipLines(["label1:", "ADD r1 r2", "test:",
+                         "BREQ r1", "loop:", "MOV r1 #5"])
+    expected = {"label1": [], "test": [], "loop": [], "main": []}
+    labelDecoder = LabelDecoder(rawInput)
+    labelDecoder._scanLabels()
+
+    assert(labelDecoder.getLabels() == expected)
+
+
+def test_splitLabels():
+    rawInput = zipLines(["MOV r4 r3", "label1:", "ADD r1 r2", "test:",
+                         "BREQ r1", "loop:", "MOV r1 #5", "MOV r1 #5"])
+    expected = {
+        "label1": [(3, "ADD r1 r2")],
+        "test": [(5, "BREQ r1")],
+        "loop": [(7, "MOV r1 #5"), (8, "MOV r1 #5")],
+        "main": [(1, "MOV r4 r3")]}
+    labelDecoder = LabelDecoder(rawInput)
+    assert(labelDecoder.getLabels() == expected)
---
+++ compiler1/module/LabelDecoder/tests/test_LabelRef.py
@@ -0,0 +1,47 @@
+import pytest
+from ..LabelDecoder import LabelDecoder
+
+
+def createRawInput(labelRef):
+    return {
+        "main": (
+            4, [
+                (1, lambda: 1),
+                (1, lambda: labelRef("loop", 1, "main")),
+                (1, lambda: 1),
+                (1, lambda: 1),
+            ]
+        ),
+        "hi": (
+            5, [
+                (1, lambda: 1),
+                (1, lambda: 1),
+                (1, lambda: 1),
+                (1, lambda: 1),
+                (1, lambda: labelRef("main", 4, "hi")),
+            ]),
+        "loop": (
+            3, [
+                (1, lambda: 1),
+                (1, lambda: labelRef("hi", 1, "loop")),
+                (1, lambda: labelRef("loop", 2, "loop")),
+            ]
+        ),
+    }
+
+
+def test_labelRef():
+    labelDecoder = LabelDecoder([])
+    rawInput = createRawInput(labelDecoder.labelRef)
+    labelDecoder.setParsedLabels(rawInput)
+    res = labelDecoder.decodeLabelRef()
+    assert(res["main"][1][1] == (1, 7))
+    assert(res["hi"][1][4] == (1, -9))
+    assert(res["loop"][1][1] == (1, -7))
+    assert(res["loop"][1][2] == (1, -3))
+
+
+def test_labelRefAssertion():
+    labelDecoder = LabelDecoder([])
+    with pytest.raises(AssertionError):
+        labelDecoder.decodeLabelRef()
