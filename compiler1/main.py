5a03db80a840e70df1f5eb18116a12aa04c5af61
MunsMan <hendrik.munske@me.com>
2021-06-19 14:54:59 +0000

    adding output to main

--- compiler1/main.py
+++ compiler1/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-from module import FileReader, throwError, LabelDecoder, Parser
+from module import FileReader, throwError, LabelDecoder, Parser, Output
 
 
 def main():
@@ -11,12 +11,17 @@ def main():
     labelDecoder = LabelDecoder(fileReader.lines)
     parsedLabels = Parser(labelDecoder.labels, labelDecoder.labelRef).parse()
     labelDecoder.parsedLabels = parsedLabels
+    output = []
     for label in parsedLabels:
         print(label)
         _, instructions = parsedLabels[label]
         for _, instruction in instructions:
             for inst in instruction():
                 print("\t0x%04X" % inst)
+                output.append(inst)
+    out = Output(output)
+    out.bin()
+    out.hex()
 
 
 if __name__ == "__main__":
