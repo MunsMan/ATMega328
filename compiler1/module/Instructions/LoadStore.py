358f443ca6caa972b5dc25083a62f398ac425419
MunsMan <hendrik.munske@me.com>
2021-07-05 18:30:22 +0000

    updating load from RegisterPointer adding faster loadIndirect

--- compiler1/module/Instructions/LoadStore.py
+++ compiler1/module/Instructions/LoadStore.py
@@ -6,28 +6,31 @@ from .instructions import mapInstructions
 from .RegisterManager import RegisterManager
 
 
-# ToDo: Need to be tested!
+# ToDo: Need to be tested for miss usage!
 def LOAD(args: LineParser):
     rd = args.rd
     rr = args.rr
     if not checkRegister(rd):
         throwError(5, True, rd)
     rd = getRegister(rd)
-    RegisterManager.setRegister(rd)
 
     if Addr.check(rr):
         rr: Addr = Addr(rr)
         instructions = mapInstructions("lds")(rd, rr.value)
+        RegisterManager.setRegister(rd)
         return 1, lambda: [instructions]
 
     if checkImmediate(rr):
         instructions = loadImmediate(rd, getImmediate(rr))
+        RegisterManager.setRegister(rd)
         return len(instructions), lambda: instructions
 
     rawPointer: str = rr.replace("+", "").replace("-", "")
     instructions = []
     if checkRegisterPointer(rawPointer):
-        if len(rr) == 2:
+        if len(rr) == 1:
+            instructions.append(mapInstructions(f"ld{rr.lower()}")(rd))
+        elif len(rr) == 2:
             if rr[0] == '-':
                 instructions.append(mapInstructions(f"ld{rr[1].lower()}d")(rd))
             elif rr[1] == '+':
@@ -35,11 +38,12 @@ def LOAD(args: LineParser):
         elif len(rr) == len(rawPointer):
             ptr = getRegisterPointer(rawPointer)
             if checkDirect(rawPointer):
-                instructions += loadDirect(rr, rd)
+                instructions += loadDirect(ptr, rd)
             else:
-                instructions += loadIndirect(rr, rd)
+                instructions += loadIndirect(ptr, rd)
         else:
             throwError()
+        RegisterManager.setRegister(rd)
         return len(instructions), lambda: instructions
 
 
@@ -48,27 +52,51 @@ def checkDirect(rawPointer):
     return rawPointer in direct
 
 
-def loadIndirect(ptr: int, rd: int) -> List[int]:
-    instructions = []
-    r1 = RegisterManager.getFreeRegister()
-    r2 = RegisterManager.getFreeRegister()
-    instructions.append(mapInstructions("mov")(r1, 26))
-    instructions.append(mapInstructions("mov")(r2, 27))
-    instructions.append(mapInstructions("mov")(26, ptr))
-    instructions.append(mapInstructions("mov")(27, ptr+1))
-    instructions.append(mapInstructions("ldx")(rd))
-    instructions.append(mapInstructions("mov")(26, r1))
-    instructions.append(mapInstructions("mov")(27, r2))
-    RegisterManager.freeRegister(r1)
-    RegisterManager.freeRegister(r2)
-    return instructions
+def getFreeAddressRegisterPointer() -> int:
+    for i, x in enumerate(range(26, 32, 2)):
+        r1 = not RegisterManager.registerIsUsed(x)
+        r2 = not RegisterManager.registerIsUsed(x + 1)
+        if r1 and r2:
+            return i
+    return -1
 
 
-def loadDirect(ptr: int, rd: int) -> List[int]:
+def mapRegisterPointerLower(rptr: str):
     ptrMap = {
         26: 'x',
         28: 'y',
         30: 'z'
     }
-    direct = ptrMap[ptr]
+    return ptrMap[rptr]
+
+
+def loadIndirect(ptr: int, rd: int) -> List[int]:
+    arp = getFreeAddressRegisterPointer()
+    instructions = []
+
+    if ptr % 2 != 0:
+        throwError()
+
+    if arp == -1:
+        r1 = RegisterManager.getFreeRegister()
+        r2 = RegisterManager.getFreeRegister()
+        instructions.append(mapInstructions("mov")(r1, 26))
+        instructions.append(mapInstructions("mov")(r2, 27))
+        instructions.append(mapInstructions("mov")(26, ptr))
+        instructions.append(mapInstructions("mov")(27, ptr+1))
+        instructions.append(mapInstructions("ldx")(rd))
+        instructions.append(mapInstructions("mov")(26, r1))
+        instructions.append(mapInstructions("mov")(27, r2))
+        RegisterManager.freeRegister(r1)
+        RegisterManager.freeRegister(r2)
+    else:
+        instructions.append(mapInstructions("mov")(ptr, 26 + arp * 2))
+        instructions.append(mapInstructions("mov")(ptr+1, 26 + arp * 2+1))
+        instructions.append(mapInstructions(
+            f"ld{mapRegisterPointerLower(26 + arp * 2)}")(rd))
+    return instructions
+
+
+def loadDirect(ptr: int, rd: int) -> List[int]:
+    direct = mapRegisterPointerLower(ptr)
     return [mapInstructions(f"ld{direct}")(rd)]
