4aa3bc045c03dcd70a8551cdc37931ed6f8a8f4c
MunsMan <hendrik.munske@me.com>
2021-07-05 18:28:41 +0000

    adding tests

--- compiler1/module/Instructions/tests/test_LoadStore.py
+++ compiler1/module/Instructions/tests/test_LoadStore.py
@@ -1,4 +1,5 @@
 from pytest_mock import MockerFixture
+from itertools import chain
 
 from .. import LoadStore
 from ..LoadStore import LOAD
@@ -50,3 +51,87 @@ def test_loadAddress():
             assert len(expected) == numInstructions
             assert expected == instructions()
             assert RegisterManager.registerIsUsed(rd)
+
+
+def mapRegisterPointer(rptr: str) -> str:
+    rptrMap = {
+        26: 'x',
+        28: 'y',
+        30: 'z'
+    }
+    if rptr.find(":") != -1:
+        return rptrMap[int(rptr.split(":")[0].replace("r", ""))]
+    else:
+        return rptr.lower()
+
+
+def test_registerPointerDirect():
+    rds = range(0, 32)
+    rrs = ['X', 'Y', 'Z'] + \
+        list(map(lambda x: f"r{x}:r{x+1}", range(26, 32, 2)))
+    for rd in rds:
+        for rr in rrs:
+            args = LineParser(f"LD r{rd} {rr}", None, None, None)
+            numInstructions, instructions = LOAD(args)
+            expectecd = [mapInstructions(f"ld{mapRegisterPointer(rr)}")(rd)]
+            assert len(expectecd) == numInstructions
+            assert expectecd == instructions()
+            assert RegisterManager.registerIsUsed(rd)
+
+
+lowerRegisterPointerMap = {
+    0: 'x',
+    1: 'y',
+    2: 'z'
+}
+
+
+def test_registerPointerIndirectFree():
+    rds = range(0, 32)
+    rptrs = range(0, 26, 2)
+    for i in range(0, 2):
+        for rd in rds:
+            for rptr in rptrs:
+                for x in range(i):
+                    RegisterManager.setRegister(26+x*2)
+                    RegisterManager.setRegister(26+x*2+1)
+                args = LineParser(
+                    f"LD r{rd} r{rptr}:r{rptr+1}", None, None, None)
+                numInstructions, instructions = LOAD(args)
+                expected = [
+                    mapInstructions("mov")(rptr, 26+i*2),
+                    mapInstructions("mov")(rptr+1, 26+i*2+1),
+                    mapInstructions(f"ld{lowerRegisterPointerMap[i]}")(rd)
+                ]
+                assert len(expected) == numInstructions
+                assert expected == instructions()
+                assert RegisterManager.registerIsUsed(rd)
+                RegisterManager.freeRegister(rd)
+
+
+def test_registerPointerIndirectUsed(mocker: MockerFixture):
+    mock_getFreeAddressRegisterPointer = mocker.patch.object(
+        LoadStore, "getFreeAddressRegisterPointer")
+    mock_getFreeAddressRegisterPointer.return_value = -1
+    rds = range(0, 32)
+    rptrs = range(0, 26, 2)
+    for rd in rds:
+        for rptr in rptrs:
+            args = LineParser(
+                f"LD r{rd} r{rptr}:r{rptr+1}", None, None, None)
+            numInstructions, instructions = LOAD(args)
+            expected = [
+                mapInstructions("mov")(0, 26),
+                mapInstructions("mov")(1, 27),
+                mapInstructions("mov")(26, rptr),
+                mapInstructions("mov")(27, rptr+1),
+                mapInstructions("ldx")(rd),
+                mapInstructions("mov")(26, 0),
+                mapInstructions("mov")(27, 1),
+            ]
+            assert len(expected) == numInstructions
+            assert expected == instructions()
+            assert RegisterManager.registerIsUsed(rd)
+            assert not RegisterManager.registerIsUsed(0) or 0 == rd
+            assert not RegisterManager.registerIsUsed(1) or 1 == rd
+            RegisterManager.freeRegister(rd)
