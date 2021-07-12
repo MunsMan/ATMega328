f39adfadf66712703aec08f5b39db169decc5220
MunsMan <hendrik.munske@me.com>
2021-07-12 16:09:56 +0000

    adding reset und Error Message

--- compiler1/module/Instructions/MemoryManager.py
+++ compiler1/module/Instructions/MemoryManager.py
@@ -1,12 +1,14 @@
 from typing import Set
 from .Types import Address
 
+from ..errorHandling import throwError
+
 
 #ToDo: test
 class MemoryManager:
     mmap: Set[Address] = set()
     _start = 0x0100
-    _end = 0x08FF
+    _end = 0x0900
 
     @staticmethod
     def getMemory() -> Address:
@@ -14,6 +16,7 @@ class MemoryManager:
             if not addr in MemoryManager.mmap:
                 MemoryManager.mmap.add(addr)
                 return addr
+        throwError(19, True)
 
     @staticmethod
     def freeMemory(addr) -> None:
@@ -21,3 +24,6 @@ class MemoryManager:
 
     def checkAddress(addr: Address) -> bool:
         return addr in MemoryManager.mmap
+
+    def reset():
+        MemoryManager.mmap = set()
