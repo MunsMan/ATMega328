aecca0b4ae8542869d576a79a317ef140131548e
MunsMan <hendrik.munske@me.com>
2021-07-15 16:02:45 +0000

    adding store to LoadStore

--- compiler1/module/Instructions/LoadStore.py
+++ compiler1/module/Instructions/LoadStore.py
@@ -9,25 +9,27 @@ from .RegisterManager import RegisterManager
 def LOAD(args: LineParser):
     rd = args.rd
     rr = args.rr
+    opcode = args.opcode.lower()
     if not checkRegister(rd):
         throwError(5, True, rd)
     rd = getRegister(rd)
     RegisterManager.setRegister(rd)
 
     if Addr.check(rr):
-        return fromAddress(rd, rr)
+        return fromAddress(opcode, rd, rr)
 
     if checkImmediate(rr):
         return fromImmediate(rd, rr)
 
     rawPointer: str = rr.replace("+", "").replace("-", "")
     if checkRegisterPointer(rawPointer):
-        return fromRegisterPointer(rd, rr, rawPointer)
+        return fromRegisterPointer(opcode, rd, rr, rawPointer)
 
 
-def fromAddress(rd: int, rr: str):
+def fromAddress(opcode: str, rd: int, rr: str):
     rr: Addr = Addr(rr)
-    instructions = mapInstructions("lds")(rd, rr.value)
+    suffix = "" if rr.value < 128 else "32"
+    instructions = mapInstructions(f"{opcode}s{suffix}")(rd, rr.value)
     return 1, lambda: [instructions]
 
 
@@ -36,7 +38,7 @@ def fromImmediate(rd: int, rr: str):
     return len(instructions), lambda: instructions
 
 
-def fromRegisterPointer(rd: int, rr: str, rawPointer: str):
+def fromRegisterPointer(opcode: str, rd: int, rr: str, rawPointer: str):
     instructions = []
     if len(rawPointer) == 1:
         suffix = ""
@@ -47,15 +49,15 @@ def fromRegisterPointer(rd: int, rr: str, rawPointer: str):
         elif len(rr) > 1:
             throwError(17, True, (False, rawPointer))
         return 1, lambda: [mapInstructions(
-            f"ld{rawPointer.lower()}{suffix}")(rd)]
+            f"{opcode}{rawPointer.lower()}{suffix}")(rd)]
 
     if len(rr) != len(rawPointer):
         throwError(17, True, (True, rawPointer))
 
     if getRegisterPointer(rr) in [26, 28, 30]:
-        instructions = loadDirect(rr, rd)
+        instructions = loadDirect(opcode, rr, rd)
     else:
-        instructions = loadIndirect(rr, rd)
+        instructions = loadIndirect(opcode, rr, rd)
 
     return len(instructions), lambda: instructions
 
@@ -64,7 +66,7 @@ def mapAddressRegisterToChar(rptr: str):
     return chr(120 + (rptr - 26) // 2)
 
 
-def loadIndirect(ptr: int, rd: int) -> List[int]:
+def loadIndirect(opcode: str, ptr: int, rd: int) -> List[int]:
     ptr = getRegisterPointer(ptr)
     RegisterManager.returnRegister(rd)
     arp = RegisterManager.getFreeAddressRegister()
@@ -82,7 +84,7 @@ def loadIndirect(ptr: int, rd: int) -> List[int]:
         instructions.append(mapInstructions("mov")(r2, 27))
         instructions.append(mapInstructions("mov")(26, ptr))
         instructions.append(mapInstructions("mov")(27, ptr+1))
-        instructions.append(mapInstructions("ldx")(rd))
+        instructions.append(mapInstructions(f"{opcode}x")(rd))
         instructions.append(mapInstructions("mov")(26, r1))
         instructions.append(mapInstructions("mov")(27, r2))
         RegisterManager.returnRegister(r1)
@@ -96,6 +98,6 @@ def loadIndirect(ptr: int, rd: int) -> List[int]:
     return instructions
 
 
-def loadDirect(ptr: int, rd: int) -> List[int]:
+def loadDirect(opcode: str, ptr: int, rd: int) -> List[int]:
     direct = mapAddressRegisterToChar(getRegisterPointer(ptr))
-    return [mapInstructions(f"ld{direct}")(rd)]
+    return [mapInstructions(f"{opcode}{direct}")(rd)]
