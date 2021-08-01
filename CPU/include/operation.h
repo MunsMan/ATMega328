c001a354c6f4f604480bb96fc7fee3c1d466b79a
MunsMan <hendrik.munske@me.com>
2021-08-01 19:04:11 +0200

    refactoring

--- CPU/include/operation.h
+++ CPU/include/operation.h
@@ -2,12 +2,13 @@
 #define _OPERATION_H_
 
 #include "types.h"
+#include "cpu.h"
 
 typedef struct operation {
     unsigned clock_cycles;
-    void (*operation)(instruction_t instruction);
+    void (*operation)(cpu_t* cpu, instruction_t instruction);
 } operation_t;
 
-operation_t* decodeInstruction(instruction_t instruction);
+operation_t* decodeInstruction(instruction_t instruction, operation_t* operation);
 
 #endif  // _OPERATION_H_
