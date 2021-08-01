0f3da0e3842824e3c831d6b1211b137266424671
MunsMan <hendrik.munske@me.com>
2021-08-01 19:03:55 +0200

    renaming register -> reg

--- CPU/include/types.h
+++ CPU/include/types.h
@@ -9,7 +9,7 @@
 typedef uint16_t addr_t;
 typedef int8_t byte_t;
 typedef int16_t word_t;
-typedef int8_t register_t;
+typedef int8_t reg_t;
 typedef uint8_t bit_t;
 
 typedef uint16_t instruction_t;
