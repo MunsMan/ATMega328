a3eb289d127873d40fc6ec156fb6fa0b80b48831
MunsMan <48153666+MunsMan@users.noreply.github.com>
2021-08-23 19:30:22 +0200

    adding adc to alu

--- CPU/src/operations/alu.c
+++ CPU/src/operations/alu.c
@@ -12,6 +12,30 @@ operation_t* alu(instruction_t instruction, operation_t* operation){
     return operation;
 }
 
+void adc(cpu_t* cpu, instruction_t instruction){
+    byte_t rd = cpu->r[getRd(instruction)];
+    byte_t rr = cpu->r[getRr(instruction)];
+    bit_t C = getC(cpu);
+    byte_t r = rd + rr + C;
+
+    cpu->r[getRd(instruction)] = r;
+
+    bit_t rd3 = (rd >> 3) & 1;
+    bit_t rd7 = (rd >> 7) & 1;
+    bit_t rr3 = (rr >> 3) & 1;
+    bit_t rr7 = (rr >> 7) & 1;
+    bit_t r3 = (r >> 3) & 1;
+    bit_t r7 = (r >> 7) & 1;
+
+    rd3 * rr3 + rr3 * ~r3 + ~r3 * rd3 ? setH(cpu) : clearH(cpu);
+    rd7 * rr7 * ~r7  + ~rd7 * ~rr7 * r7 ? setV(cpu) : clearV(cpu);
+    r7 ? setN(cpu) : clearN(cpu);
+    getN(cpu) ^ getV(cpu) ? setS(cpu): clearS(cpu);
+    r == 0 ? setZ(cpu) : clearZ(cpu);
+    rd7 * rr/ + rr7 * ~r7 + ~r7 * rd7 ? setC(cpu) : clearC(cpu);
+
+}
+
 void add(cpu_t* cpu, instruction_t instruction){
     byte_t rd = cpu->r[getRd(instruction)];
     byte_t rr = cpu->r[getRr(instruction)];
@@ -33,6 +57,8 @@ void add(cpu_t* cpu, instruction_t instruction){
     getN(cpu) ^ getV(cpu) ? setS(cpu) : clearS(cpu);
     rd3 * rr3 + rr3 * ~r3 + ~r3 * rd3 ? setH(cpu) : clearH(cpu);
 }
+
+
 
 reg_t getRd(instruction_t instruction){
     return (instruction >> 4) & 0x1F;
