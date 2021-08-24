c5217d99a888881fdf07b1f9e79cd34d44c59cf0
MunsMan <48153666+MunsMan@users.noreply.github.com>
2021-08-24 18:34:34 +0200

    mask for Opcode/Instruction

--- CPU/test/operations/helper.py
+++ CPU/test/operations/helper.py
@@ -1,2 +1,29 @@
+from typing import Dict
+
+opcode = "0010 00rd dddd rrrr"
+
+
+def getBitAt(byte: int, index: int):
+    return (byte & 1 << index - 1) >> (index - 1)
+
+
+def maskOpcode(opcode: str, **kwargs: Dict[str, int]):
+    charDict = {}
+    opcode = opcode.replace(" ", "")
+    opcode = opcode.replace("_", "")
+    for bit in opcode:
+        if not bit.isdigit():
+            if bit in charDict:
+                charDict[bit] += 1
+            else:
+                charDict[bit] = 1
+    for bit in opcode:
+        if not bit.isdigit():
+            value = getBitAt(kwargs[bit], charDict[bit])
+            charDict[bit] -= 1
+            opcode = opcode.replace(bit, str(value), 1)
+    return int(opcode, 2)
+
+
 def twoOp(upcode: int, rd: int, rr: int) -> int:
     return upcode + (rd << 4) + (rr & (1 << 5)) + (rr & 0xF)
